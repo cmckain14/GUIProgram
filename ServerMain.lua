eb13caa1024ac31960b071457a4bb96a32e2ed85
cmckain14 <cmckain14@users.noreply.github.com>
2014-06-07 10:13:19 -0700

    Update ServerMain.lua
    Fixed x, added watchnumber() but did not start it.

--- ServerMain.lua
+++ ServerMain.lua
@@ -1,38 +1,40 @@
 function scp(x,y)
-	monitor.setCursorPos(x,y)
-	end
+   monitor.setCursorPos(x,y)
+end
+function watchnumber()
+end
 function watch(c,pos1,pos2)
-	if not type(pos1) == "number" or type(pos2) == "number" then
-		pos1 = tonumber(pos1)
-		pos2 = tonumber(pos2)
-		end
-	scp(pos1,pos2)
-	text = c["text"]
-	if text == nil then
-		print("You idiot!")
-		end
-	monitor.write(text)
-	local length = string.len(text)
-	local l = tonumber(length)
-	l = l + pos1
-	if c == c1 then 
-		c1[1] = pos1 
-		c1[2] = l 
-		c1["y"] = pos2 
-	elseif c == c2 then
-		c2[1] = pos1 
-		c2[2] = l 
-		c2["y"] = pos2 
-	elseif c == c3 then
-		c3[1] = pos1 
-		c3[2] = l 
-		c3["y"] = pos2 
-	elseif c == c4 then
-		c4[1] = pos1 
-		c4[2] = l 
-		c4["y"] = pos2 
-	end
-	end
+   if not type(pos1) == "number" or type(pos2) == "number" then
+      pos1 = tonumber(pos1)
+      pos2 = tonumber(pos2)
+   end
+   scp(pos1,pos2)
+   text = c["text"]
+   if text == nil then
+      print("You idiot!")
+   end
+   monitor.write(text)
+   local length = string.len(text)
+   local l = tonumber(length)
+   l = l + pos1
+   if c == c1 then
+      c1[1] = pos1
+      c1[2] = l
+      c1["y"] = pos2
+   elseif c == c2 then
+      c2[1] = pos1
+      c2[2] = l
+      c2["y"] = pos2
+   elseif c == c3 then
+      c3[1] = pos1
+      c3[2] = l
+      c3["y"] = pos2
+   elseif c == c4 then
+      c4[1] = pos1
+      c4[2] = l
+      c4["y"] = pos2
+   end
+end
 c1 = {}
 c2 = {}
 c3 = {}
@@ -53,42 +55,53 @@ watch(c1,c1x,c1y)
 watch(c2,c2x,c2y)
 watch(c3,c3x,c3y)
 watch(c4,c4x,c4y)
-x = 3
+c1["xx"] = 4
+watchnumber()
+c2["xx"] = 4
+watchnumber()
+c3["xx"] = 4
+watchnumber()
+c4["xx"] = 4
+watchnumber()
 repeat
-	event, side, xPos, yPos = os.pullEvent("monitor_touch")
-	print(event .. " => Side: " .. tostring(side) .. ", " ..
-	"X: " .. tostring(xPos) .. ", " ..
-	"Y: " .. tostring(yPos))
-	print(xPos >= c2[1])
-	print(xPos <= c2[2])
-	print(yPos == c2["y"])
-        if xPos >= c1[1] and xPos <= c1[2] and yPos == c1["y"] then
-                if x == 2 then
-			monitor.setCursorPos(c1x,c2y)
-			monitor.setTextColor(colors.green)
-			monitor.write(c1["text"])
-			x = x - 1
-			end
-		if x == 3 then
-			monitor.setCursorPos(c1x,c2y)
-			monitor.setTextColor(colors.red)
-			monitor.write(c1["text"])
-			x = x - 1
-			end
-	elseif xPos >= c2[1] and xPos <= c2[2] and yPos == c2["y"] then
-		if x == 2 then
-			monitor.setCursorPos(c2x,c2y)
-			monitor.setTextColor(colors.green)
-			monitor.write(c2["text"])
-			x = x - 1
-			end
-		if x == 3 then
-			monitor.setCursorPos(c2x,c2y)
-			monitor.setTextColor(colors.red)
-			monitor.write(c2["text"])
-			x = x - 1
-			end
-        elseif xPos >= c3[1] and xPos <= c3[2] and yPos == c3["y"] then
-        elseif xPos >= c4[1] and xPos <= c4[2] and yPos == c4["y"] then
-        end
+   event, side, xPos, yPos = os.pullEvent("monitor_touch")
+   print(event .. " => Side: " .. tostring(side) .. ", " ..
+   "X: " .. tostring(xPos) .. ", " ..
+   "Y: " .. tostring(yPos))
+   print(xPos >= c2[1])
+   print(xPos <= c2[2])
+   print(yPos == c2["y"])
+   if xPos >= c1[1] and xPos <= c1[2] and yPos == c1["y"] then
+      if c1["xx"] == 2 then
+         monitor.setCursorPos(c1x,c2y)
+         monitor.setTextColor(colors.green)
+         monitor.write(c1["text"])
+         watchnumber()
+         c1["xx"] = c1["xx"] - 1
+      end
+      if c1["xx"] == 3 then
+         monitor.setCursorPos(c1x,c2y)
+         monitor.setTextColor(colors.red)
+         monitor.write(c1["text"])
+         watchnumber()
+         c1["xx"] = c1["xx"] - 1
+      end
+   elseif xPos >= c2[1] and xPos <= c2[2] and yPos == c2["y"] then
+      if c2["xx"] == 2 then
+         monitor.setCursorPos(c2x,c2y)
+         monitor.setTextColor(colors.green)
+         monitor.write(c2["text"])
+         watchnumber()
+         c2["xx"] = c2["xx"] - 1
+      end
+      if c2["xx"] == 3 then
+         monitor.setCursorPos(c2x,c2y)
+         monitor.setTextColor(colors.red)
+         monitor.write(c2["text"])
+         watchnumber()
+         c2["xx"] = c2["xx"] - 1
+      end
+   elseif xPos >= c3[1] and xPos <= c3[2] and yPos == c3["y"] then
+   elseif xPos >= c4[1] and xPos <= c4[2] and yPos == c4["y"] then
+   end
 until x == 1
