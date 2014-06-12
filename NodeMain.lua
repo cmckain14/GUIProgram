ce85dc7685271f14658f1417efdf544283f2e8e0
cmckain14 <cmckain14@users.noreply.github.com>
2014-06-12 15:56:41 -0700

    Create NodeMain.lua

---
+++ NodeMain.lua
@@ -0,0 +1,24 @@
+function send(m)
+	rednet.send(0,m)
+	end
+while true do
+	id,message = rednet.receive()
+	sleep(.1)
+	if id == 0 and message ~= nil then
+		if message == "1st place Packet!" then
+			send("Good job!")
+		elseif message == "Are you still there?" then
+			send("Yep!")
+		elseif message == "Reboot." then
+			send("Ok.")
+			reboot = true
+			return false
+		else 
+			send("Sorry, didn't catch that.")
+			end
+		end
+	end
+if reboot == true then
+	os.reboot()
+	end
+			
