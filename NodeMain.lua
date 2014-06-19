83026d7336d25ec5de061ebcfe52de146af76fad
cmckain14 <cmckain14@users.noreply.github.com>
2014-06-18 22:13:26 -0700

    Update NodeMain.lua
    Major update

--- NodeMain.lua
+++ NodeMain.lua
@@ -1,5 +1,22 @@
  -- Functions
-function Set (list)
+function getGUI(name,pastebin)
+	local file = io.open(name, "r")      
+	if file == nil then
+		resp = http.get("http://pastebin.com/raw.php?i="..pastebin)
+		if resp then
+			handler=io.open(name,"w")
+			handler:write(resp.readAll())
+			handler:close()
+			os.loadAPI(name)
+		else
+			error("Unable to get GUI.")
+			end
+		end
+	if file ~= nil then
+		os.loadAPI(name)
+		end
+end
+function Set(list)
 	local set = {}
 	for _, l in ipairs(list) do set[l] = true end
 	return set
@@ -13,7 +30,57 @@ function color(color) --After each use, please use 'color(white)' to reset color
 		return false
 		end
 end
-function intro1()
+function isOdd(n)
+	if (n % 2 == 0) then
+		return false
+	else
+		return true
+		end
+end
+function barfor1v2()
+	local percent = math.random(1,5)
+	repeat 
+		bar:update(percent)
+		if percent <= 25 and percent >= 0 then
+			change = math.random(1,49)
+		elseif percent <= 50 and percent >= 25 then
+			change = math.random(1,39)
+		elseif percent <= 75 and percent >= 50 then
+			change = math.random(1,24)
+		elseif percent < 100 and percent >= 75 then
+			change = math.random(1,14)
+			end
+		percent = percent + change
+		local s = math.random(1,5)
+		if e == 1 then
+			e = e + 1
+			s = 1
+			end
+		if percent >= 100 and e == 0 then
+			percent = 100
+			e = 1
+			end
+		sleep(s)
+	until e == 2
+end
+function termsignal()
+	tsactive = true
+	while tsactive do 
+		local event, scancode = os.pullEvent("key")
+		if scancode == 28 then
+			tsactive = false
+			end
+		end
+end
+function createintro2tables()
+	i2first = {}
+	i2second = {}
+	i2third = {}
+	i2first["bcolor"] = gray
+	i2second["bcolor"] = gray
+	i2third["bcolor"] = gray
+end
+function intro1v1()
 	term.clear()
 	term.setCursorPos(1,1)
 	introduction = string.format("%s %q", "Booting up", name)
@@ -36,37 +103,104 @@ function intro1()
 	booting[2] = false
 	booting[3] = false
 end
+function intro1v2()
+	term.clear()
+	bar = gui.createBar("intro")
+	t = 25
+	local w,h = term.getSize()
+	if isOdd(w) then
+		w = w + 1
+		end
+	local w = math.floor(w / 2 * .5)
+	local h = math.floor(h / 2 + .5)
+	--t = tonumber(t)
+	bar:draw(w,h,t,white,green,true,black,white)
+	term.setCursorPos(1,1)
+	e = 0
+	sleep(2)
+	parallel.waitForAny( barfor1v2, termsignal )
+	percent = nil
+	change = nil
+	booting = {}
+	booting[1] = false
+	booting[2] = false
+	booting[3] = false
+end
 function intro2()
 	term.clear()
 	term.setCursorPos(1,1)
+	local Header = window.create(term.current(),1,1,51,3)
+	Header.setBackgroundColor(green)
+	Header.clear()
+	local t,a = 51,3
+	local t2,a2 = 1,2
+	local text = string.format("%s %s", name, "Client: Intro Screen") 
+	Header.setCursorPos(math.ceil((t / 2) - (text:len() / 2)), a2)
+	Header.write(text)
+	term.setBackgroundColor(black)
 	intro2 = false
 	e = 1
+	createintro2tables()
+	local First = window.create(term.current(),1,5,23,1)
+	First.setBackgroundColor(i2first["bcolor"])
+	First.clear()
+	local Second = window.create(term.current(),1,7,23,1)
+	Second.setBackgroundColor(i2second["bcolor"])
+	Second.clear()
+	local Third = window.create(term.current(),1,9,23,1)
+	Third.setBackgroundColor(i2third["bcolor"])
+	Third.clear()
+	First.write("First Message:")
+	fx,fy = First.getCursorPos()
+	Second.write("Response Sent:")
+	sx,sy = Second.getCursorPos()
+	Third.write("Instructions:")
+	tx,ty = Third.getCursorPos()
 	repeat
 		if e <= 9 then
-			term.setCursorPos(51,1)
+			Header.setCursorPos(51,1)
 		elseif e >= 10 and e <= 99 then
-			term.setCursorPos(50,1)
+			Header.setCursorPos(50,1)
 		elseif e == 100 then
 			e = 1
 			end
-		print(e)
+		Header.write(e)
 		e = e + 1
 		b1 = tostring(booting[1])
 		if b1 == "true" then
-			b1 = "true "
+			b1 = "     true"
+			b1c = green
+		elseif b1 == "false" then
+			b1 = "    false"
+			b1c = red
 			end
 		b2 = tostring(booting[2])
 		if b2 == "true" then
-			b2 = "true "
+			b2 = "     true"
+			b2c = green
+		elseif b2 == "false" then
+			b2 = "    false"
+			b2c = red
 			end
 		b3 = tostring(booting[3])
 		if b3 == "true" then
-			b3 = "true "
+			b3 = "      true"
+			b3c = green
+		elseif b3 == "false" then
+			b3 = "     false"
+			b3c = red
 			end
-		term.setCursorPos(1,1)
-		print("First Message: "..b1)
-		print("Response Sent: "..b2)
-		print("Directions Received: "..b3)
+		term.setCursorPos(1,5)
+		term.setBackgroundColor(colors.black)
+		First.setCursorPos(fx,fy)
+		First.setTextColor(b1c)
+		First.write(b1)
+		Second.setCursorPos(sx,sy)
+		Second.setTextColor(b2c)
+		Second.write(b2)
+		Third.setCursorPos(tx,ty)
+		Third.setTextColor(b3c)
+		Third.write(b3)
 		sleep(1)
 		if intro22 == true then
 			intro2 = true
@@ -74,9 +208,12 @@ function intro2()
 		if booting[1] == true and booting[2] == true and booting[3] == true then
 			intro22 = true
 			end
-	until intro2 == true
+	until intro3 == true
+end
+function clientscreen()
 end
 function startuping()
+	sleep(3)
 	rednet.open(rnside)
 	needmessage = true
 	while needmessage do
@@ -101,39 +238,38 @@ function send(m)
 	rednet.send(serverid,m)
 	end
  -- Terminal Colors
-colors = {}
 if term.isColor() then
-	colors["white"] = colors.white
-	colors["black"] = colors.black
-	colors["lgray"] = colors.lightGray
-	colors["gray"] = colors.gray
-	colors["brown"] = colors.brown
-	colors["yellow"] = colors.yellow
-	colors["orange"] = colors.orange
-	colors["red"] = colors.red
-	colors["magenta"] = colors.magenta
-	colors["purple"] = colors.purple
-	colors["blue"] = colors.blue
-	colors["lblue"] = colors.lightBlue
-	colors["cyan"] = colors.cyan
-	colors["lime"] = colors.lime
-	colors["green"] = colors.green
+	white = colors.white
+	black = colors.black
+	lgray = colors.lightGray
+	gray = colors.gray
+	brown = colors.brown
+	yellow = colors.yellow
+	orange = colors.orange
+	red = colors.red
+	magenta = colors.magenta
+	purple = colors.purple
+	blue = colors.blue
+	lblue = colors.lightBlue
+	cyan = colors.cyan
+	lime = colors.lime
+	green = colors.green
 end
 if not term.isColor() then
-	colors["lgray"] = colors.white
-	colors["gray"] = colors.black
-	colors["brown"] = colors.white
-	colors["yellow"] = colors.white
-	colors["orange"] = colors.white
-	colors["red"] = colors.white
-	colors["magenta"] = colors.white
-	colors["purple"] = colors.white
-	colors["green"] = colors.white
-	colors["blue"] = colors.black
-	colors["lblue"] = colors.white
-	colors["cyan"] = colors.white
-	colors["lime"] = colors.white
-	colors["green"] = colors.white
+	lgray = colors.white
+	gray = colors.black
+	brown = colors.white
+	yellow = colors.white
+	orange = colors.white
+	red = colors.white
+	magenta = colors.white
+	purple = colors.white
+	green = colors.white
+	blue = colors.black
+	lblue = colors.white
+	cyan = colors.white
+	lime = colors.white
+	green = colors.white
 end
  -- Config
 name = os.getComputerLabel()
@@ -142,9 +278,10 @@ m1 = 1 --Server->Client
 r1 = 1.1--Client->Server
 serverid = 0
  -- Intro Code
+gg = getGUI("gui","qz7KGw3R")
 m1 = tostring(m1)
 r1 = tostring(r1)
-intro1()
+intro1v2()
 parallel.waitForAll( intro2, startuping )
  -- Receive Commands
 while true do
@@ -166,4 +303,4 @@ while true do
 if reboot == true then
 	os.reboot()
 	end
-			
+		
